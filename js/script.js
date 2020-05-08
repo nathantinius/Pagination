085cfbdec4c726b7c66f5ca1a8169e4ae921f32a
Nathan Tinius <nathantinius@gmail.com>
2020-05-08 12:21:21 -0500

    MAJOR UPDATE: Finished @todo for Exceeds Expectations. Refactored Code for clarity.

--- js/script.js
+++ js/script.js
@@ -3,98 +3,155 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
- * @constant studentsList: gets the children of the ul element
- * @constant pageSize: sets the amount of students per page
- * @type {string} pageNumber: used to keep track of the page number 
- * in the showPage function.
- * @constant getUI: this gets the div where the pagination will live
- * @constant setDiv: creates a new div to live inside getUI
-***/
-
-const studentsList = document.querySelector('.student-list').children;
-const pageSize = 10;
-let pageNumber = 0;
-const getUI = document.querySelector('.page');
+// Student List Area - These Variables get and create the student list area from/to the DOM
+const studentsList = document.querySelector('.student-list');
+let students = studentsList.children;
+const getPage = document.querySelector('.page');
 const setDiv = document.createElement('div');
 setDiv.className = "pagination";
+const ul = document.createElement('ul');
+
+//Page Settings - These variables set up the page conditions
+const pageSize = 10;
+
+// Search Bar Area - These Variables get and create the search area from/to the DOM
+const getHeader = document.querySelector('.page-header');
+let searchDiv = document.createElement('div');
+searchDiv.className = 'student-search';
+let searchBar = document.createElement('input');
+let searchButton = document.createElement('button');
+
+// No Results Area - These Variables get and create the No Results area from/to the DOM
+const noResults = document.createElement('h3');
+const noResultsDiv = document.createElement('div');
+
 
 /***
  * showPage Function -
- * @constant startRange: sets the beginning of the page size
- * @constant endRange: sets the end of the page size
- * loop through the students list showing only the students that
- * exist between startRange and endRange
- * Use display none as we don't want to destroy list items
-***/
-
-const showPage = (students, pageNumber) => {
+ * Displays the studentsList as a paginated result 
+ ***/
+function showPage(students, pageNumber) {
 
    let startRange = (pageNumber - 1) * pageSize;
-   console.log(startRange);
-
    let endRange = pageNumber * pageSize;
-   console.log(endRange);
 
    for (i = 0; i < students.length; i++) {
+      students[i].style.display = 'none';
       if (i >= startRange && i < endRange) {
          students[i].style.display = '';
-      } else {
-         students[i].style.display = 'none';
       }
    }
 }
 
-// Defaults to page one.
-showPage(studentsList, 1);
-
 /***
- * appendPageLinks Function -
- * @type {string} html: builds the html to append to setDiv
- * @constant totalPages: uses math to round to the next pageNumber 
- * loop through the totalPages creating a link for each new page
- * store the newly created pages to setDiv
- * append setDiv to getUI
-***/
-
-const appendPageLinks = () => {
-   let html = '<ul>';
-   const totalPages = Math.ceil(studentsList.length / pageSize);
-
-   for (i = 0; i < totalPages; i++) {
-      pageNumber = i + 1;
-      html += `<li><a class="link${i} pageLink" onclick="showPage(studentsList, ${pageNumber}); setActive(event)">${pageNumber}</a></li>`;
-   }
-   html += '</ul>';
-
-   setDiv.innerHTML = html;
-   getUI.append(setDiv);
-}
-
-appendPageLinks();
-
-/***
- * @constant pages: used to set event listener
- * loop through all the pageLinks
- * remove active class
- * add active to the event.target
+ * buildPageButton Function -
+ * Creates the page links based on the volume 
+ * of students and either returns a list of 
+ * page buttons or returns a no results found
+ * if the pageTotal is 0
+ * 
+ * The addEventListener is a part of this function
  ***/
+function buildPageButtons(students, pageTotal) {
+   noResults.innerHTML = "";
+   if (pageTotal > 0) {
+      for (i = 1; i < pageTotal + 1; i++) {
+         let li = document.createElement('li');
+         let pageLink = document.createElement('a');
+         if (i == 1) {
+            pageLink.className = 'active';
+         }
+         pageLink.textContent = i;
 
-const pages = document.querySelectorAll('.pageLink');
+         pageLink.addEventListener('click', (event) => {
+            const buttonClick = event.target;
+            showPage(students, buttonClick.textContent);
+            setActive(buttonClick);
+         })
 
-const setActive = (event) => {
-   for (i = 0; i < pages.length; i++) {
-      console.log(pages[i].classList);
-      pages[i].classList.remove("active");
+         li.appendChild(pageLink);
+         ul.appendChild(li);
+      }
+      setDiv.appendChild(ul);
+   } else {
+      noResults.innerHTML = "Sorry your search returned 0 results";
+      setDiv.append(noResults);
    }
-   const activeLink = event.target;
-   activeLink.classList.add("active");
+
+   return setDiv;
+}
+
+function appendPageLinks(students) {
+   // Adds the pagination links to the page and displays the correct list items
+   const pageTotal = Math.ceil(students.length / pageSize)
+
+   // Remove any current pagination links
+   ul.innerHTML = "";
+
+   const div = buildPageButtons(students, pageTotal);
+   getPage.appendChild(div);
+
+   showPage(students, 1);
+}
+
+function setActive(buttonClick) {
+   const pageLinks = ul.querySelectorAll('li');
+   for (i = 0; i < pageLinks.length; i++) {
+      const link = pageLinks[i].children[0];
+      console.log(link)
+      if (buttonClick === link) {
+         link.className = 'active';
+      } else {
+         link.className = '';
+      }
+   }
 }
 
 /***
- * @todo Append search bar
- * @todo Write serch function
- */
+ * AppendSearch Function -
+ * Creates the search elements and
+ * appends them to the DOM
+ ***/
+function appendSearch() {
+   searchBar.placeholder = "Search for students..."
+   searchBar.addEventListener('keyup', () => {
+      searchStudents();
+   })
+   searchButton.className = "search-button";
+   searchButton.textContent = "Search";
+   searchDiv.appendChild(searchBar);
+   searchDiv.appendChild(searchButton);
+   return getHeader.appendChild(searchDiv);
+}
+
+
+// The addEventListener for the Search Button
+searchButton.addEventListener('click', (event) => {
+   searchStudents();
+})
+
+/***
+ * SearchStudents Function -
+ ***/
+function searchStudents() {
+   const searchText = searchBar.value.toLowerCase();
+   console.log(searchText)
+   let results = [];
+
+   for (i = 0; i < students.length; i++) {
+      const student = students[i];
+      const studentName = student.querySelector('h3').textContent;
+      if (studentName.includes(searchText)) {
+         console.log(studentName);
+         results.push(student);
+         student.style.display = '';
+      } else {
+         student.style.display = 'none';
+      }
+   }
+   appendPageLinks(results);
+
+}
+
+appendPageLinks(students);
+appendSearch();
